7286a37fb2f7a576209c8319c28942471377839d
Josh Mandel <jmandel@alum.mit.edu>
2023-10-30 13:03:50 -0500

    Add footer logo relative link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
-	import '../app.css';
+  import "../app.css";
 </script>
 
 <main>
-	<slot />
+  <slot />
 </main>
-<br/>
+<br />
 <footer>
-	<img src="logo2.png" />
-SMART on FHIR, {new Date().toISOString().slice(0,4)}
+  <!-- svelte-ignore a11y-missing-attribute -->
+  <img src="./logo2.png" />
+  SMART on FHIR, {new Date().toISOString().slice(0, 4)}
 </footer>
 
 <style>
-	main {
-		min-height: calc(100% - 20em);
-		    padding-top: 1px;
-
-	}
-	footer {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
-	img {
-		max-width: 25vmin;
-		max-height: 25vmin;
-		display: block;
-	}
+  main {
+    min-height: calc(100% - 20em);
+    padding-top: 1px;
+  }
+  footer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  img {
+    max-width: 25vmin;
+    max-height: 25vmin;
+    display: block;
+  }
 </style>
